7e31055f2491895be41f4dd85667069aa4bc07a7
Trịnh Việt Đức <ductv@onenet.vn>
2026-06-04 09:49:47 +0700

    Fix: resolve Swagger Failed to load API definition error

--- apps/api/src/ONENET.WebAPI/Program.cs
+++ apps/api/src/ONENET.WebAPI/Program.cs
@@ -67,7 +67,10 @@ builder.Services.AddSwaggerGen(c =>
     // Set the comments path for the Swagger JSON and UI.
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
+    }
 });
 
 // Configure JWT Authentication
